012073c632afe7bbec1465efb263581ff273047b
spar3chang3 <baker.daryk@ic.edu>
2025-01-03 18:20:09 -0600

    Add some server auth for users

--- cmd/serverauth/serverauth.go
+++ cmd/serverauth/serverauth.go
@@ -48,15 +48,50 @@ func getUserByToken(token string) (*User, error) {
 	return user, nil
 }
 
+func addNewUserToken(user User, ip string) (AccessToken, error) {
+	newToken, err := generateNewAccessToken()
+	if err != nil {
+		return AccessToken{}, err
+	}
+
+	newAccess := AccessToken{
+		Token:   newToken,
+		TokenIP: ip,
+	}
+
+	user.AccessTokens = append(user.AccessTokens, newAccess)
+
+	return newAccess, nil
+}
+
 func checkDuplicateUser(UserName string) bool { //ed participates
 	_, exists := accessMap[UserName]
 	return exists
 }
 
-func checkUserLogin(UserName string, encodedPassword string) (bool, error) {
-	user, err := userMap[UserName]
-	if err {
-		return false, errors.New("could not validate user login")
+func checkUserTokenLogin(token string, ip string) (bool, error) {
+	user, exists := accessMap[token]
+	if !exists {
+		return false, errors.New("access token not found")
+	}
+	//We should probably come up with a better way, but right now the map only provides a user pointer from token
+	//This means the AccessToken array still needs to be iterated over match said token
+	//However, how many tokens will there reasonably be? A max of 10 maybe? eh, sounds like too much refactoring
+	tokenArray := user.AccessTokens
+
+	for _, accessToken := range tokenArray { //this means for [however long this obj is], iterate with accessToken as current index
+		if accessToken.Token == token && accessToken.TokenIP == ip {
+			return true, nil
+		}
+	}
+
+	return false, errors.New("associated IP no longer valid")
+}
+
+func checkUserLogin(UserName string, encodedPassword string, ip string) (bool, error, AccessToken) {
+	user, exists := userMap[UserName]
+	if !exists {
+		return false, errors.New("could not validate user login"), AccessToken{}
 	}
 
 	decodedPassword, _ := b64.StdEncoding.DecodeString(encodedPassword)
@@ -65,10 +100,16 @@ func checkUserLogin(UserName string, encodedPassword string) (bool, error) {
 	passErr := bcrypt.CompareHashAndPassword([]byte(user.PasswordHash), bytePassword)
 
 	if passErr != nil {
-		return false, errors.New("incorrect username and/or password")
+		return false, errors.New("incorrect username and/or password"), AccessToken{}
 	}
 
-	return true, nil
+	newAccess, err := addNewUserToken(*user, ip)
+
+	if err != nil {
+		return false, err, AccessToken{}
+	}
+
+	return true, nil, newAccess
 }
 
 func addNewUser(UserName string, encodedPassword string, ip string) error {
